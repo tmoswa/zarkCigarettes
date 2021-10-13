9e2f3b104ac7aadcd6b3c1e795beab552f142049
Timothy Moswa <timmymoswa11@gmail.com>
2021-10-13 13:20:40 +0200

    ntms update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -641,7 +641,7 @@
                         <p>Bay 27, Harrow Bussiness Park</p>
                         <p>Msasa, Harare Zimbabwe</p><br>
                         <p><strong>Email: </strong> info@zarkcigerrates.com</p>
-                        <p><strong>Tel No.: </strong>+263 242 4466 40/96</p>
+                        <p><strong>Telephone No.: </strong>+263 242 4466 40/96</p>
                         <p><strong>Cell / Whatsapp No.: </strong>+263 731 400 000</p>
 
                     </div>
